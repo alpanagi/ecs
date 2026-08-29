75de93582c8962c65046d4a097005d5c8816e278
Alexandros Panagiotakis <dev@alpanagi.com>
2026-08-29 20:18:11 +0200

    Add mutable pointer to struct helper.

---
+++ src/utils/protocols/mutable_pointer.zig
@@ -0,0 +1,40 @@
+const std = @import("std");
+
+pub fn validate(T: anytype) bool {
+    const type_info = @typeInfo(@TypeOf(T));
+    const pointer = switch (type_info) {
+        .pointer => |pointer| pointer,
+        else => return false,
+    };
+
+    const child_info = @typeInfo(pointer.child);
+    if (child_info != .@"struct") return false;
+
+    return true;
+}
+
+test "validate: accepts a pointer to a struct" {
+    const Data = struct { data: u32 };
+    var data: Data = .{ .data = 11 };
+
+    try std.testing.expect(validate(&data));
+}
+
+test "validate: rejects a struct value" {
+    const Data = struct { data: u32 };
+    const data: Data = .{ .data = 12 };
+
+    try std.testing.expect(!validate(data));
+}
+
+test "validate: rejects a pointer to a non-struct" {
+    var data: u32 = 13;
+
+    try std.testing.expect(!validate(&data));
+}
+
+test "validate: rejects a pointer to a slice" {
+    var data: [2]u32 = .{ 14, 15 };
+
+    try std.testing.expect(!validate(&data));
+}
