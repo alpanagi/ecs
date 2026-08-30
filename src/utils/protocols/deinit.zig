58dd4a59050f66e88d5eb4616f3e470545240326
Alexandros Panagiotakis <dev@alpanagi.com>
2026-08-30 19:39:20 +0200

    Deinit protocol now gives a compile time error for malformed deinit functions. It also now forces always two arguments to the deinit function.

--- src/utils/protocols/deinit.zig
+++ src/utils/protocols/deinit.zig
@@ -3,32 +3,23 @@ const std = @import("std");
 const deinit_function_name = "deinit";
 
 pub fn validate(T: type) bool {
+    const error_message = "Does not implement Deinit protocol: " ++ @typeName(T);
+
     if (@typeInfo(T) != .@"struct") return false;
     if (!std.meta.hasFn(T, deinit_function_name)) return false;
 
     const deinit_info = @typeInfo(@TypeOf(@field(T, deinit_function_name))).@"fn";
-    if (deinit_info.return_type != void) return false;
+    if (deinit_info.return_type != void) @compileError(error_message);
 
-    const params = switch (deinit_info.params.len) {
-        1, 2 => deinit_info.params,
-        else => return false,
-    };
-
-    if (params[0].type != *T) return false;
-    if (params.len == 2 and params[1].type != std.mem.Allocator) return false;
+    const params = deinit_info.params;
+    if (params.len != 2) @compileError(error_message);
+    if (params[0].type != *T) @compileError(error_message);
+    if (params[1].type != std.mem.Allocator) @compileError(error_message);
 
     return true;
 }
 
-test "validate: accepts a valid single parameter deinit" {
-    const Type = struct {
-        pub fn deinit(_: *@This()) void {}
-    };
-
-    try std.testing.expect(validate(Type));
-}
-
-test "validate: accepts a valid two parameter deinit" {
+test "validate: accepts a valid deinit" {
     const Type = struct {
         pub fn deinit(_: *@This(), _: std.mem.Allocator) void {}
     };
@@ -44,38 +35,6 @@ test "validate: rejects a non-struct type" {
 
 test "validate: rejects a type without a deinit" {
     const Type = struct { data: u32 };
-
-    try std.testing.expect(!validate(Type));
-}
-
-test "validate: rejects a single parameter deinit with an invalid argument" {
-    const Type = struct {
-        pub fn deinit(_: u32) void {}
-    };
-
-    try std.testing.expect(!validate(Type));
-}
-
-test "validate: rejects a two parameter deinit with an invalid second argument" {
-    const Type = struct {
-        pub fn deinit(_: *@This(), _: u32) void {}
-    };
-
-    try std.testing.expect(!validate(Type));
-}
-
-test "validate: rejects deinit with invalid return type" {
-    const Type = struct {
-        pub fn deinit(_: *@This()) !void {}
-    };
-
-    try std.testing.expect(!validate(Type));
-}
-
-test "validate: rejects deinit with more than 2 parameters" {
-    const Type = struct {
-        pub fn deinit(_: *@This(), _: std.mem.Allocator, _: *@This()) void {}
-    };
 
     try std.testing.expect(!validate(Type));
 }
