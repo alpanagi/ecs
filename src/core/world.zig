b6c2a5a04b185610e1aa73f177e8fb206681e41e
Alexandros Panagiotakis <dev@alpanagi.com>
2026-08-31 15:22:38 +0200

    Update world to hold contexts.

--- src/core/world.zig
+++ src/core/world.zig
@@ -1,1 +1,31 @@
-pub const World = struct {};
+const std = @import("std");
+
+const Box = @import("../utils/box.zig").Box;
+const ContextId = @import("../modules/contexts/module.zig").ContextId;
+
+pub const World = struct {
+    contexts: std.AutoHashMapUnmanaged(ContextId, Box) = .empty,
+
+    pub fn init() World {
+        return World{};
+    }
+
+    pub fn deinit(self: *World, allocator: std.mem.Allocator) void {
+        var it = self.contexts.valueIterator();
+        while (it.next()) |box| box.deinit(allocator);
+        self.contexts.deinit(allocator);
+    }
+};
+
+test "deinit: calls the box's deinit" {
+    const allocator = std.testing.allocator;
+
+    var world = World.init();
+    defer world.deinit(allocator);
+
+    const Context = struct { data: u32 };
+    const context = try allocator.create(Context);
+    context.* = .{ .data = 11 };
+
+    try world.contexts.put(allocator, ContextId.fromType(Context), Box.fromOwnedPointer(context));
+}
