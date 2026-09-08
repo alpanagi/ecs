1f7f3b4dd002512e25e03a563451d3eebc38c9af
Alexandros Panagiotakis <dev@alpanagi.com>
2026-09-09 00:24:40 +0200

    Add systems parameter pass through functions.

--- src/modules/systems/parameter.zig
+++ src/modules/systems/parameter.zig
@@ -11,4 +11,21 @@ pub const Systems = struct {
         const resource = Resource(SystemsState).fromWorld(allocator, world).value;
         return Systems{ .state = resource };
     }
+
+    pub fn addGroup(
+        self: Systems,
+        allocator: std.mem.Allocator,
+        group_name: []const u8,
+    ) void {
+        self.state.addGroup(allocator, group_name);
+    }
+
+    pub fn add(
+        self: Systems,
+        allocator: std.mem.Allocator,
+        group_name: []const u8,
+        system: anytype,
+    ) void {
+        self.state.add(allocator, group_name, system);
+    }
 };
